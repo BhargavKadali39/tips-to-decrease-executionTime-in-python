6611b57ec67813981d890866fbf5de51ede46728
Bhargav <bhargavkadali39@gmail.com>
2021-10-19 12:27:45 +0530

    Create importing-required-functions.py

---
+++ importing-required-functions.py
@@ -0,0 +1,14 @@
+import time
+start_time = time.time()
+#------------------------- code area -----------------------------------------
+import math
+
+i = 9.67
+
+print(math.ceil(i))
+print(math.floor(i))
+
+#------------------------- code area -----------------------------------------
+end_time = time.time()
+total_time = end_time - start_time
+print("Execution time is :",total_time)
