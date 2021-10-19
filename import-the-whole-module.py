d83bfeffa6f7468bcea33363274e5d13cfef0aab
Bhargav <bhargavkadali39@gmail.com>
2021-10-19 12:26:22 +0530

    Update import-the-whole-module.py

--- import-the-whole-module.py
+++ import-the-whole-module.py
@@ -1,1 +1,15 @@
 
+import time
+start_time = time.time()
+#------------------------- code area -----------------------------------------
+import math
+
+i = 9.67
+
+print(ceil(i))
+print(floor(i))
+#------------------------- code area -----------------------------------------
+end_time = time.time()
+total_time = end_time - start_time
+print("Execution time is :",total_time)
+
