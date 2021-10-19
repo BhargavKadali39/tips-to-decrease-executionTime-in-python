287c37f48efc50eb346d86c5e76b47fd7d363d08
Bhargav <bhargavkadali39@gmail.com>
2021-10-19 12:40:45 +0530

    Update using-while-loop.py

--- using-while-loop.py
+++ using-while-loop.py
@@ -1,1 +1,16 @@
+import time
+start_time = time.time()
+#------------------------- code area -----------------------------------------
 
+i=0
+while True:
+    print(i)
+    i += 1
+    if i == 10:
+        print('over')
+        break
+
+#------------------------- code area -----------------------------------------
+end_time = time.time()
+total_time = end_time - start_time
+print("Execution time is :",total_time)
