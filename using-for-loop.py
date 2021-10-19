3337222d121be4e0594c4c5bbda6f0d09f900d6e
Bhargav <bhargavkadali39@gmail.com>
2021-10-19 12:35:26 +0530

    Update using-for-loop.py

--- using-for-loop.py
+++ using-for-loop.py
@@ -1,1 +1,13 @@
+import time
+start_time = time.time()
+#------------------------- code area -----------------------------------------
 
+for i in range(10):
+print(i)
+if i == 9:
+    print('over')
+
+#------------------------- code area -----------------------------------------
+end_time = time.time()
+total_time = end_time - start_time
+print("Execution time is :",total_time)
